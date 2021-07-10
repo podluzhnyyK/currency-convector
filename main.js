bac4123104046113a42325a9c355fa6a5c49ad65
podluzhnyyK <podluzhnyy94@mail.ru>
2021-07-10 19:43:39 +0500

    настройки отправки письма

--- main.js
+++ main.js
@@ -1,88 +1,54 @@
 // Объект с курсами валют
 const rates = {};
 
-const elementUSD = document.querySelector('[data-value="USD"]');
-const elementEUR = document.querySelector('[data-value="EUR"]');
-const elementGBP = document.querySelector('[data-value="GBP"]');
-
 const input = document.querySelector('#input');
 const select = document.querySelector('#select');
 const result = document.querySelector('#result');
 
 const courses = document.querySelector('#courses');
 
-
-
-
-
-
 async function getCurrencies() {
-    const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
-    const data = await response.json();
-    const result = await data;
-
-    console.log(result);
-    console.log(result.Valute);    
-
-    // rates.Valute = result.Valute;
-    // console.log(rates);
-    // rates.EUR = result.Valute.EUR;
-    //  rates.GBP = result.Valute.GBP;
-    //  console.log(rates.GBP);
+  const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+  const data = await response.json();
+  const result = await data;
 
     
-
-    // elementUSD.textContent = rates.USD.Value.toFixed(2);
-    // elementEUR.textContent = rates.EUR.Value.toFixed(2);
-    // elementGBP.textContent = rates.GBP.Value.toFixed(2);
-
-    for (let param in result.Valute) { 
+  // наполнение данными
+  for (let param in result.Valute) { 
              
-      const balloonTemplate = document.querySelector('#card').content.querySelector('.card-body');
-      const cardElement = balloonTemplate.cloneNode(true); 
-      const select = document.querySelector('#select')     
+    const balloonTemplate = document.querySelector('#card').content.querySelector('.card-body');
+    const cardElement = balloonTemplate.cloneNode(true);       
 
-      let currency = result.Valute[param];
-      rates[`${param}`] = Number(currency.Value.toFixed(2));
+    let currency = result.Valute[param];
+    rates[`${param}`] = Number(currency.Value.toFixed(2));
 
-      cardElement.querySelector('.course-item-title').textContent = param;
-      cardElement.querySelector('.course-item-value').textContent = currency.Value.toFixed(2);
-      let option = `<option value="${param}">${param} — ${currency.Name}</option>`;
+    cardElement.querySelector('.course-item-title').textContent = param;
+    cardElement.querySelector('.course-item-value').textContent = currency.Value.toFixed(2);
+    let option = `<option value="${param}">${param} — ${currency.Name}</option>`;
       
-      courses.append(cardElement);
-      select.insertAdjacentHTML("beforeend", option);
-
+    courses.append(cardElement);
+    select.insertAdjacentHTML("beforeend", option);      
       
-      
-      // цвет для информера
+    // цвет для информера
     if (currency.Value > currency.Previous) {
       cardElement.querySelector('.course-item-value').classList.add('top');
-  } else {
-    cardElement.querySelector('.course-item-value').classList.add('bottom');
-  }
-
-  
-
-  
-
+    } else {
+      cardElement.querySelector('.course-item-value').classList.add('bottom');
     }
 
-    console.log(rates);
+  }  
     
 }
 
 getCurrencies();
 
-
+// расчет конверсии
 input.oninput = convertValue;
 select.oninput = convertValue;
 
-
 function convertValue() {
   result.value = (parseFloat(input.value) / rates[select.value]).toFixed(2);    
-    
 }
-
 
 // слайдер
 const swiper = new Swiper('.swiper-container', {
@@ -100,3 +66,31 @@ const swiper = new Swiper('.swiper-container', {
     delay: 0,
   },
 });
+
+// Отправка данных на сервер
+function send(event, php){
+  console.log("Отправка запроса");
+  event.preventDefault ? event.preventDefault() : event.returnValue = false;
+  var req = new XMLHttpRequest();
+  req.open('POST', php, true);
+  req.onload = function() {
+    if (req.status >= 200 && req.status < 400) {
+    json = JSON.parse(this.response); 
+        console.log(json);
+          
+        // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+        if (json.result == "success") {
+          // Если сообщение отправлено
+          alert("Сообщение отправлено");
+        } else {
+          // Если произошла ошибка
+          alert("Ошибка. Сообщение не отправлено");
+        }
+      // Если не удалось связаться с php файлом
+      } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+  
+  // Если не удалось отправить запрос. Стоит блок на хостинге
+  req.onerror = function() {alert("Ошибка отправки запроса");};
+  req.send(new FormData(event.target));
+  
+  }
